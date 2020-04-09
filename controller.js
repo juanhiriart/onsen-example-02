c18afdc36b0bf3175a887e21f6ada3a175c5928e
Don Juanito <juanhiriart@gmail.com>
2020-04-09 18:24:17 +0100

    Place story added

--- controller.js
+++ controller.js
@@ -21,7 +21,6 @@ function getLocation()
       var lon = position.coords.longitude;
       showMap(lat, lon);
     });
-
   }
   else
   {
@@ -51,6 +50,29 @@ function showMap(lat, lon)
 
 };
 
+var locations = [
+  {name: "Juan's flat",
+  lat: 53.30058397483567,
+  lng: -2.1007242278630938,
+  description: "This is were Dr Juan lives."},
+  {name: "Bollington rec",
+  lat: 53.299155,
+  lng: -2.103137,
+  description: "This is were Dr Juan plays."},
+  {name: "Somewhere in the canal",
+  lat: 53.301264,
+  lng: -2.103073,
+  description: "This is were Dr Juan walks."}
+]
+
+function tellPlaceStory()
+{
+  var closest = 0;
+  document.getElementById('placeName').innerHTML = locations[closest].name;
+  document.getElementById('disToPlace').innerHTML = 0000;
+  document.getElementById('placeDescription').innerHTML = locations[closest].description;
+}
+
 function showPointOfInterest(map)
 {
   var pointsOfInterest = [
@@ -62,7 +84,8 @@ function showPointOfInterest(map)
       type: 'info'}
   ];
 
-  for (var i = 0; i < pointsOfInterest.length; i++) {
+  for (var i = 0; i < pointsOfInterest.length; i++)
+  {
     var marker = new google.maps.Marker({
     position: pointsOfInterest[i].position,
     icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png',
@@ -70,6 +93,27 @@ function showPointOfInterest(map)
     });
   };
 };
+
+//https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-in-your-web-apps.html
+function distance(lat1, lon1, lat2, lon2, unit)
+{
+  var radlat1 = Math.PI * lat1/180;
+  var radlat2 = Math.PI * lat2/180;
+  var radlon1 = Math.PI * lon1/180;
+  var radlon2 = Math.PI * lon2/180;
+  var theta = lon1-lon2;
+  var radtheta = Math.PI * theta/180;
+  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  dist = Math.acos(dist);
+  dist = dist * 180/Math.PI;
+  dist = dist * 60 * 1.1515;
+  if (unit=="K") { dist = dist * 1.609344 }
+  if (unit=="N") { dist = dist * 0.8684 }
+  return dist;
+}
+
+
+
 
 //https://developers.google.com/maps/documentation/javascript/custom-markers
 
