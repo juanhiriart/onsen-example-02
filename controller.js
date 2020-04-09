ac816dfd884043bd96e85fc12e7e8d0abc25648e
Don Juanito <juanhiriart@gmail.com>
2020-04-09 17:02:14 +0100

    Added mapPage

--- controller.js
+++ controller.js
@@ -12,7 +12,7 @@ window.fn.load = function(page) {
     .then(menu.close.bind(menu));
 };
 
-window.onload = function()
+function getLocation()
 {
   if(navigator.geolocation)
   {
@@ -28,6 +28,8 @@ window.onload = function()
     document.write('Your browser does not support Geolocation');
   }
 }
+
+//var infoWindow;
 
 function showMap(lat, lon)
 {
@@ -46,7 +48,33 @@ function showMap(lat, lon)
   map: map,
   title: 'Found you!'
   });
-}
+
+  var pointsOfInterest = [
+    { position: new google.maps.LatLng(53.30058397483567, -2.1007242278630938),
+      type: 'info'}
+  ];
+
+  for (var i = 0; i < pointsOfInterest.length; i++) {
+    var marker = new google.maps.Marker({
+    position: pointsOfInterest[i].position,
+    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/parking_lot_maps.png',
+    map: map
+    });
+  };
+
+};
+
+
+
+  /*
+  var pos = {lat: 53.30058397483567, lng: -2.1007242278630938};
+  infoWindow = new google.maps.InfoWindow;
+  infoWindow.setPosition(pos);
+  infoWindow.setContent('Point 1');
+  infoWindow.open(map);
+  */
+
+
 
 
 
